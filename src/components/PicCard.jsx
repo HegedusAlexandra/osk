576d6f306f03848945c1512e53314f17f438f298
HegedusAlexandra <alexandra.hegedus@vamosoft.hu>
2024-03-04 17:59:18 +0100

    added cards to sum

--- src/components/PicCard.jsx
+++ src/components/PicCard.jsx
@@ -5,23 +5,25 @@ export default function PicCard({
   atr,
   product,
   handleDecrement,
-  handleIncrement
+  handleIncrement,
+  screen
 }) {
   return (
     <div className="w-[100%] overflow-hidden">
       <img
         alt="topPic1"
         src={product ? product.url : src}
-        className={`imgBoxShadow w-[100%] h-[70vh] bg-stone-300 rounded-sm object-cover ${
+        className={`imgBoxShadow w-[100%] ${screen === 'cart' ? 'h-[18vh]' : 'h-[70vh]'} bg-stone-300 rounded-sm object-cover ${
           product ? product.atr : atr
         }`}
       />
       {product && (
         <div
-          className="flex flex-row justify-between items-center font-montserrat text-black absolute w-[26.5vw] h-[6vh] text-[2vh] bg-white -translate-y-[5vh] rounded-sm"
+          className="flex flex-row justify-between items-center font-montserrat text-black absolute w-[40vw] h-[6vh] text-[2vh] bg-white -translate-y-[5vh] rounded-sm"
           key={product.id}
         >
           <h2 className="text-[2vh]">{product.name}</h2>
+          <h2 className="text-[2vh]">{product.price} Ft</h2>
           <div className="flex justify-center items-center flex-row pr-[2vh] gap-[1vh]">
             <button
               className="flex justify-center items-center size-[2vh] bg-stone-200 rounded-full"
--- src/pages/ProductSummary.jsx
+++ src/pages/ProductSummary.jsx
@@ -1,40 +1,72 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Menu from "../components/Menu";
+import Footer from "./Footer";
+import { NavLink } from "react-router-dom";
+import PicCard from "../components/PicCard";
+import { useDispatch } from "react-redux";
+import {
+  incrementQuantity,
+  decrementQuantity
+} from "../redux/slices/productSlice";
 
 export default function ProductSummary() {
   const products = useSelector((state) => state.products.products);
   const selectedProducts = products.filter((product) => product.quantity > 0);
 
+  const dispatch = useDispatch();
+
+  const handleIncrement = (id) => {
+    dispatch(incrementQuantity(id));
+  };
+
+  const handleDecrement = (id) => {
+    dispatch(decrementQuantity(id));
+  };
+
+
   return (
-    <div className="schoolbook_square">
-      <Menu screen={'sum'}/>
-      <div className="headline2">
-        <h2>choosen products</h2>
+    <div className="w-[100%]">
+      <Menu screen={"sum"} />
+      <div className="flex flex-col justify-between items-between w-[100%] px-[1.5vw] font-montserrat">
+        <div className="headline2">
+          <h2>KOSÁR</h2>
+        </div>
+        <div className="m-[1vw]">
+          {selectedProducts.length > 0 ? (
+            <ul className="font-montserrat">
+              {selectedProducts.map((product) => (
+                <li key={product.id}>
+                  <div className="flex flex-row gap-[1vw]">
+                    <div className="w-[80%] h-[20vh]">
+                      <PicCard handleIncrement={handleIncrement} handleDecrement={handleDecrement} screen={"cart"} product={product} />
+                    </div>{" "}
+                    Összesen: {product.quantity * product.price} Ft
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              The cart is empty yet, please select someting nice from the{" "}
+              <NavLink className="text-orange-600 font-bold" to="/productlist">
+                product list
+              </NavLink>{" "}
+            </p>
+          )}
+        </div>
+        <div className="bottomContainer">
+          <h2 className="finalRes">
+            SUM: 
+            { products
+              .map((el) => el.price * el.quantity)
+              .reduce((acc, cur) => acc + cur, 0)} Ft
+          </h2>
+          <button className="bottomButton">I want it</button>
+        </div>
       </div>
-      <div className="result">
-        {selectedProducts.length > 0 ? (
-          <ul>
-            {selectedProducts.map((product) => (
-              <li key={product.id}>
-                {product.name} - {product.quantity} db - {product.price} Ft/db -
-                Összesen: {product.quantity * product.price} Ft
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No choosen products, we will starve as you are :(</p>
-        )}
-      </div>
-      <div className="bottomContainer">
-        <h2 className="finalRes">
-          SUM:
-          {products
-            .map((el) => el.price * el.quantity)
-            .reduce((acc, cur) => acc + cur, 0)}
-        </h2>
-        <button className="bottomButton">I want it</button>
-      </div>
+
+      <Footer />
     </div>
   );
 }
