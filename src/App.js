90b3a43a03322f1ca592987686aaae34e66d0741
HegedusAlexandra <alexandra.hegedus@vamosoft.hu>
2024-03-05 14:41:57 +0100

    style correction

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Menu from "./components/Menu";
 import Clouds from "./components/Clouds";
 import Main from "./pages/Main";
+import MobileMenu from "./components/MobileMenu";
 
 function App() {
   return (
     <div className="flex flex-col justify-between items-center w-[100%] h-[500vh] bg-gradient-to-br from-sky-200 to-sky-500 overflow-hidden">
+      {window.innerWidth > 780 ? <Menu screen={"home"} /> : <MobileMenu />}
       <Main/>
     </div>
   );
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Landing from "./Landing";
-import Menu from "../components/Menu";
 import Contact from "./Contact";
 import AboutUs from "./AboutUs";
 import Testimonials from "./Testimonials";
 import Brands from "./Brands";
 import TopPics from "./TopPics";
 import Footer from "./Footer";
-import MobileMenu from "../components/MobileMenu";
 
 export default function Main() {
   return (
-    <div className="absolute z-2 w-[100%] h-[500vh] bg-transparent">
-      {window.innerWidth > 780 ? <Menu screen={"home"} /> : <MobileMenu />}
+    <div className=" w-[100%] h-[500vh] bg-transparent">
       <Landing />
       <TopPics />
       <Brands />
