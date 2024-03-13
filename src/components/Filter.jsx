f36bbd322031dc45f12733a1c9a6a290bf45950b
HegedusAlexandra <alexandra.hegedus@vamosoft.hu>
2024-03-13 14:10:57 +0100

    wip:resp

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -5,10 +5,10 @@ import FilterWindow from './FilterWindow'
 export default function Filter() {
   return (
     <div className='flex flex-row w-[100%] pt-[10vh] pb-[2vh] h-[4vh]'>
-    <div className='flex justify-start w-[50%] h-[4vh]'>
+    {window.innerWidth > 780 && <div className='flex justify-start w-[50%] h-[4vh]'>
         <FilterWindow />
-    </div>
-    <div className='flex justify-end w-[50%] h-[4vh] pr-[1vw]'>
+    </div>}
+    <div className='flex justify-end md:w-[50%] w-[96%] h-[4vh] pr-[1vw]'>
         <Dropdown type={'sort'} />
     </div>
     </div>
